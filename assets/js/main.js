6bf183daad75a610f709455068cf14634afaa4dc
adityakk29 <adityakk29@gmail.com>
2021-06-07 16:50:22 +0530

    change logo

--- assets/js/main.js
+++ assets/js/main.js
@@ -104,7 +104,80 @@ typewriter = setupTypewriter(typewriter);
 
 typewriter.type();
 
-$(document).ready(function(){
+// shim layer with setTimeout fallback
+// window.requestAnimFrame = (function () {
+//   return function (callback) {
+//     window.setTimeout(callback, 1000);
+//   };
+// })();
+
+// $(document).ready(function () {
+
+//   var $shadow = $('#my_name');
+//   var shadowLimit = 7;
+//   var moveEvent = ('ontouchstart' in document.documentElement) ? "touchmove" : "mousemove";
+
+//   (function animloop() {
+//     requestAnimFrame(animloop);
+
+//     $(window).bind(moveEvent, function (ev) {
+//       var $this = $(this);
+//       var w = $this.width();
+//       var h = $this.height();
+//       var center = { x: w / 2, y: h / 2 };
+
+//       var evX = (moveEvent == 'touchmove') ? ev.originalEvent.touches[0].clientX : ev.clientX;
+//       var evY = (moveEvent == 'touchmove') ? ev.originalEvent.touches[0].clientY : ev.clientY;
+
+//       var shadowX = (center.x - evX) / 10;
+//       var shadowY = (center.y - evY) / 10;
+
+//       shadowX = (shadowX > shadowLimit) ? shadowLimit : shadowX;
+//       shadowX = (shadowX < shadowLimit * -1) ? shadowLimit * -1 : shadowX;
+//       shadowY = (shadowY > shadowLimit) ? shadowLimit : shadowY;
+//       shadowY = (shadowY < shadowLimit * -1) ? shadowLimit * -1 : shadowY;
+
+//       $shadow.css({ textShadow: Math.ceil(shadowX) + 'px ' + Math.ceil(shadowY) + 'px ' + Math.abs(shadowX * shadowY) / 100 + 'px rgba(197, 168, 0, 1)' });
+
+
+//     });
+//   })();
+// });
+
+const p = document.querySelector('#my_name');
+
+const clamp = (a, m, n) => {
+  const max = Math.max(m, n);
+  const min = Math.min(m, n);
+
+  return Math.max(min, Math.min(max, a));
+};
+
+const MAX_SHADOW_OFFSET = 15;
+
+const paint = (x, y) => {
+  const r = p.getBoundingClientRect();
+  const o = Math.min(r.width, r.height, MAX_SHADOW_OFFSET); // compute max shadow offset
+
+  const mx = clamp(x, r.left - o, r.right + o); // clamp mouse coordinates within the shadow projection bounding box.
+  const my = clamp(y, r.top - o, r.bottom + o);
+  const px = r.right - r.width / 2; // compute element bb midpoints.
+  const py = r.bottom - r.height / 2;
+  const nx = (mx - px) / (r.right - r.left + 2 * o); // project mouse position relative to the bounding box to [-.5, .5];
+  const ny = (my - py) / (r.bottom - r.top + 2 * o);
+
+  requestAnimationFrame(() => {
+    p.style.textShadow = `${-1 * nx * o}px ${-1 * ny * o}px 0px rgba(197, 168, 0, 1)`;
+  });
+};
+
+document.addEventListener('mousemove', (e) => paint(e.clientX, e.clientY), {
+  passive: true
+});
+
+
+
+$(document).ready(function () {
   $('.owl-carousel').owlCarousel({
     loop: true,
     center: true,
@@ -215,7 +288,7 @@ $(document).ready(function(){
   on('click', '.scrollto', function (e) {
     if (select(this.hash)) {
       e.preventDefault()
-
+      console.log('lol')
       let body = select('body')
       if (body.classList.contains('mobile-nav-active')) {
         body.classList.remove('mobile-nav-active')
